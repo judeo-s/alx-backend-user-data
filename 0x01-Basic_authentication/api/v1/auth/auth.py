34bc8b2b9d2ee5e74ef6a44dfc30974df0129d6a
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-04 05:10:16 +0000

    feat: implemented the rest of the tasks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,42 +1,39 @@
-#!/usr/bin/env python3
-"""
-Manages API Authentication.
-"""
 
-from flask import request
-from typing import List, Union
+#!/usr/bin/env python3
+
+"""This module implements base class for Authentication mechanisms."""
+
+from typing import List, TypeVar, Union
+
+User = TypeVar("User")
 
 
 class Auth:
-    """A class to handle and manage API authentication
-    """
+    """Auth class to manage the API authentication."""
 
-    def __init__(self):
-        """A constructor for the Auth class
-        """
-        pass
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """A method that checks authentication
-        """
+    @staticmethod
+    def require_auth(path: str, excluded_paths: List[str]) -> bool:
+        """Require authentication for API paths except for excluded paths."""
         if not path or not excluded_paths:
             return True
+
         path = path.rstrip("/") + "/"
 
         for exc_path in excluded_paths:
+            # perform a simple a regex to accept paths matching the pattern
             if path.startswith(exc_path.rstrip("*")):
                 return False
+
         return path not in excluded_paths
 
-    def authorization_header(self, request=None) -> Union[str, None]:
-        """A method that checks the authorization header
-        """
+    @staticmethod
+    def authorization_header(request=None) -> Union[str, None]:
+        """Return the value of the Authorization header."""
         if not request:
             return None
 
         return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> User:
-        """Return the current user.
-        """
+        """Return the current user."""
         return None
