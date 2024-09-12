c5423e6fb97356773c4a87dd0c9e310daa644f7f
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-12 17:21:15 +0000

    feat: implemented a view function to handle the registration of users to the database

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 """
 A basic Flask application
 """
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -13,6 +15,21 @@ def index():
     """The view function for the index page.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """The view function to handle register users to the database.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return {"message": "email already registered"}, 400
+
+    return {"email": email, "message": "user created"}
 
 
 if __name__ == "__main__":
