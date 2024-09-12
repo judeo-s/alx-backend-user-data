1ba9b8e2a26d11a81c964394d66229c9f9768f65
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-12 11:13:45 +0000

    feat: created module to handle operations in the database. Implemented the function to add a user to the database

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,42 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """ An instance method to add a user to the database
+        """
+        if not email or not hashed_password:
+            return None
+        db_session = self._session
+        db_user = User(email=email, hashed_password=hashed_password)
+        db_session.add(db_user)
+        db_session.commit()
+
+        return db_user
