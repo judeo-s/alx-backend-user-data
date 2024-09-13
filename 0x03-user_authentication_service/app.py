60d272c5a0939c8b65dc81f088379495fcfa8bb3
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-09-13 00:04:53 +0000

    feat: implemented a lot of features

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 A basic Flask application
 """
-from flask import jsonify, Flask, request, make_response, abort
+from flask import jsonify, Flask, request, make_response, abort, redirect
 from auth import Auth
 
 
@@ -43,8 +43,49 @@ def login():
         abort(401)
 
     response = make_response({"email": f"{email}", "message": "logged in"})
-    response.set_cookie("session_id", AUTH._generate_uuid())
+    response.set_cookie("session_id", AUTH.create_session(email))
     return response
+
+
+@app.route("/sessions", methods=["DELETE"])
+def logout():
+    """A view function that handles logout requests of logged in users.
+    """
+    session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
+
+    if AUTH.delete_session(session_id):
+        redirect("/")
+    else:
+        abort(403)
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """A view function to retrieve profile of registered users.
+    """
+    session_id = request.cookies.get("session_id")
+    email = AUTH.user_email_by_session_id(session_id)
+    if email:
+        return {"email": f"{email}"}
+    else:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """A view function for registered users to request reset tokens
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return {"email": email, "reset_token": reset_token}
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,10 +29,7 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """A function to register users into the database.
         """
-        try:
-            found_user = self._db.find_user_by(email=email)
-        except NoResultFound:
-            found_user = None
+        found_user = self.search_user(email=email)
 
         if found_user:
             raise ValueError(f"User {email} already exists")
@@ -45,12 +42,8 @@ class Auth:
         """An instance method to check if the password matches the hashed
         password in the datbase.
         """
-        found_user = None
-        try:
-            found_user = self._db.find_user_by(email=email)
-        except InvalidRequestError:
-            return False
-        except NoResultFound:
+        found_user = self.search_user(email=email)
+        if not found_user:
             return False
 
         return bcrypt.checkpw(
@@ -66,12 +59,53 @@ class Auth:
     def create_session(self, email: str) -> str:
         """A method used to create a sesison for a registered user.
         """
+        user = self.search_user(email=email)
+        session_id = None
+        if user:
+            session_id = Auth._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
+
+    def delete_session(self, session_id: str) -> bool:
+        """ A method used to delete session_id
+        """
+        user = self.search_user(session_id=session_id)
+        if user:
+            self._db.update_user(user.id, session_id=None)
+            return True
+        else:
+            return False
+
+    def user_email_by_session_id(self, session_id: str) -> str:
+        """A method used to return the email of a user using the session ID
+        """
+        user = self.search_user(session_id=session_id)
+        if user:
+            return user.email
+        else:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """A method used to reset password token
+        """
+        user = self.search_user(email=email)
+        if user:
+            token = self._generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        else:
+            raise ValueError("user does not exist")
+
+    def search_user(self, **kwargs):
+        """A method to find user from details
+        """
         found_user = None
         try:
-            found_user = self._db.find_user_by(email=email)
+            print(kwargs)
+            found_user = self._db.find_user_by(**kwargs)
         except InvalidRequestError:
             return None
         except NoResultFound:
             return None
-
-        return Auth._generate_uuid()
+        return found_user
